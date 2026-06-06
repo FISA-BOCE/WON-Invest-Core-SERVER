d220f7ef2dc8f7778ed3c4f05058611e6018e245
chaeyu <chaeyu2004@naver.com>
2026-06-06 23:11:40 +0900

    Fix: 동일한 엔터티, 레포지토리 충돌 수정

--- src/main/java/com/woorifisa/won_invest_core_server/domain/account/repository/InvestOrderLedgerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.woorifisa.won_invest_core_server.domain.account.repository;
-
-import com.woorifisa.won_invest_core_server.domain.account.model.InvestOrderLedger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvestOrderLedgerRepository extends JpaRepository<InvestOrderLedger, Long> {
-}
