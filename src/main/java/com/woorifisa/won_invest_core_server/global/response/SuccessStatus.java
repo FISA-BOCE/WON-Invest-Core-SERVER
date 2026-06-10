a1711cf44f82297ed95e9528a0dc6d8e6fe92ca3
caminobelllo <alstpgml0812@gmail.com>
2026-06-11 02:24:22 +0900

    Feat: 자동 투자 체결 이력 조회 성공 코드 정의

--- src/main/java/com/woorifisa/won_invest_core_server/global/response/SuccessStatus.java
+++ src/main/java/com/woorifisa/won_invest_core_server/global/response/SuccessStatus.java
@@ -14,7 +14,8 @@ public enum SuccessStatus {
     ETF_PRODUCT_SYNCED(HttpStatus.OK, "ETF_200_001", "ETF 상품 마스터 동기화가 완료되었습니다."),
 
     INVEST_ACCOUNT_CREATED(HttpStatus.CREATED, "INVEST_201_001", "증권계좌 개설이 완료되었습니다."),
-    INVEST_ACCOUNT_ETF_DETAILS_FETCHED(HttpStatus.OK, "INVEST_200_001", "보유 ETF 상세 조회가 완료되었습니다.");
+    INVEST_ACCOUNT_ETF_DETAILS_FETCHED(HttpStatus.OK, "INVEST_200_001", "보유 ETF 상세 조회가 완료되었습니다."),
+    INVEST_AUTO_INVEST_EXECUTION_HISTORIES_FETCHED(HttpStatus.OK, "INVEST_200_010", "ETF 자동 투자 체결 이력 조회가 완료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
